4a5cae5fa0778eddacf74ea21a8cfc560330db89
cyrilou242 <cdecatheu@hey.com>
2025-10-01 00:51:16 +0200

    implement demo app

---
+++ App.java
@@ -0,0 +1,43 @@
+/// usr/bin/env jbang "$0" "$@" ; exit $?
+
+// import a dependency with Jbang-like imports
+//DEPS org.apache.commons:commons-math3:3.6.1
+
+// NOTE: this Jeamlit import is only here to help the Intellij Jbang plugin - it is not strictly necessary
+// NOTE: make sure to install the plugin for highlighting and completion https://plugins.jetbrains.com/plugin/18257-jbang
+//DEPS io.jeamlit:jeamlit:0.23.0
+
+import io.jeamlit.core.Jt;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class App {
+
+    public static void main(String[] args) {
+        Jt.title("Hello World !").use();
+
+        Jt.markdown("## A simple click app").use();
+
+        // initialize state - values in the session state are maintained at each update
+        Jt.sessionState().putIfAbsent("count", 0);
+
+        // if button is clicked, increment the count value
+        if (Jt.button("Click me").use()) {
+            Jt.sessionState().computeIfPresentInt("count", (k, v) -> v + 1);
+        }
+
+        // display the count
+        int count = Jt.sessionState().getInt("count");
+        boolean plural =  count > 1 ;
+        Jt.markdown("The button was clicked **%s** time%s".formatted(count, plural ? "s" : ""))
+                .use();
+
+
+        // Use the imported library apache commons-math
+        Jt.divider().use();
+        Jt.markdown("## Using imported apache math dependency").use();
+        double[] values = {1.2, 2.3, 3.4, 4.5, 5.6};
+        DescriptiveStatistics stats = new DescriptiveStatistics(values);
+        double stdDev = stats.getStandardDeviation();
+        Jt.text("Computed statistic with Apache commons-math: " + stdDev).use();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # jeamlit-example-standalone-vanilla
-Example Jeamlit app that you can fork. Standalone vanilla / Jbang setup.
+Example Jeamlit app that you can fork. 
+**Standalone vanilla java / Jbang setup.** .
+
+## Requirements
+
+`Java JDK >= 21`
+
+## Installation
+
+Install Jeamlit:
+
+### With [JBang](https://www.jbang.dev/) (recommended):
+```bash
+# find the latest version on Maven Central: https://central.sonatype.com/artifact/io.jeamlit/jeamlit
+JEAMLIT_VERSION=0.23.0
+
+jbang app install io.jeamlit:jeamlit:${JEAMLIT_VERSION}:all
+```
+
+### With curl:
+
+```bash
+# find the latest version on Maven Central: https://central.sonatype.com/artifact/io.jeamlit/jeamlit
+JEAMLIT_VERSION=0.23.0
+
+curl -L -o jeamlit.jar https://repo1.maven.org/maven2/io/jeamlit/jeamlit/${JEAMLIT_VERSION}/jeamlit-${JEAMLIT_VERSION}-all.jar
+```
+
+## Run 
+
+### with jbang
+```bash
+jeamlit run App.java
+```
+
+### with java CLI
+```
+java -jar jeamlit.jar run App.java
+```
+
+## Development
+- Make sure to install the [JBang plugins](https://plugins.jetbrains.com/plugin/18257-jbang) to benefit from code completion and highlighting.
+- Edit the file, the app will reload automatically.
+  For instance, add:
+  ```java
+  Jt.markdown("**OMG THE HOT-RELOAD IS REAL**").use();
+  ```
+    
+
+
